ef0c647f367463fd5c0bb7f0e77bf4f953bcea93
ba138 <basitalyshah51214@gmail.com>
2026-08-22 14:09:02 +0500

    feat: Implement LoginScreen layout with MyText widgets for user prompts

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zavora/contants/app_colors.dart';
+import 'package:zavora/contants/myText.dart';
 
 class LoginScreen extends StatefulWidget {
   const new({super.key});
@@ -10,6 +12,40 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Center(
+                child: MyText(
+                  text: "Zavora",
+                  size: 60,
+                  weight: FontWeight.w600,
+                  color: AppColors.onSecondary,
+                ),
+              ),
+              const SizedBox(height: 30),
+              MyText(
+                text: "Welcome Back!",
+                color: AppColors.background,
+                size: 24,
+                weight: FontWeight.bold,
+              ),
+              MyText(
+                text: 'Please Login or Sign Up To Continue Our App.',
+                color: const Color(0xFF777777),
+                size: 16,
+                weight: FontWeight.w400,
+                lineHeight: 1.45,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
