fd7a9e5ac6f73272ff0811889052e0dd315e92f0
ba138 <basitalyshah51214@gmail.com>
2026-08-24 22:36:54 +0500

    feat: Update ShopScreen layout with order buttons and styling

--- lib/features/shop/screens/shop_screen.dart
+++ lib/features/shop/screens/shop_screen.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:zavora/contants/app_colors.dart';
+import 'package:zavora/contants/myText.dart';
+import 'package:zavora/contants/my_button.dart';
 
 class ShopScreen extends StatelessWidget {
   const ShopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Shop',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+    return Scaffold(
+      backgroundColor: AppColors.onPrimary,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  MyText(
+                    text: "My Order",
+                    color: AppColors.background,
+                    size: 20,
+                    weight: FontWeight.bold,
+                  ),
+                  const SizedBox(width: 22),
+                  Expanded(
+                    child: MyBorderButton(
+                      buttonText: "ongoing",
+                      onTap: () {},
+                      bgColor: Colors.transparent,
+                      textColor: AppColors.background,
+                      borderColor: AppColors.background,
+                      radius: 8,
+                      height: 40,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+
+                  Expanded(
+                    child: MyButton(
+                      buttonText: "Complete",
+                      onTap: () {},
+                      radius: 8,
+                      height: 40,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
